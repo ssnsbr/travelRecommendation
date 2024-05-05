877438654016f0395afd4f94ab6efaaabc500768
sasan saberi <sasan.saberi.m@gmail.com>
2024-05-05 15:30:44 +0330

    final

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,22 +1,62 @@
 
 
 
-async function readfile() {
-    const response = await fetch("travel_recommendation_api.json");
-    const movies = await response.json();
-    console.log(movies);
-}
+const filepath = "./travel_recommendation_api.json";
 
 
-function search(query) {
 
-    const q = query.toLowerCase()
-    if (q == "beach") {
+// const jsondata = readfile();
+// console.log(jsondata)
 
-    } else if (q == "temple"){
+document.getElementById("searchButton").addEventListener("click", () => {
 
-    } else {
+    fetch(filepath) // fetch editable c:
+        .then(function (response) {
+            const jsondata = response.json();
+            return jsondata
+        }).then(function (jsondata) {
 
-    }
+            const result_div = document.getElementById("result_div");
+            const q = document.getElementById("insertedSearch").value.toLowerCase();
+            if (q == "beaches") {
+                const result_one = jsondata[q][0]
+                const result_two = jsondata[q][1]
+                result_div.innerHTML = getHtml(result_one) + getHtml(result_two);
+                   
+            } else if (q == "temples") {
+                const result_one = jsondata[q][0]
+                const result_two = jsondata[q][1]
+                result_div.innerHTML = getHtml(result_one) + getHtml(result_two);
 
-}
+            }  else if (q == "countries") {
+                const result_one = jsondata[q][0]["cities"][0]
+                const result_two = jsondata[q][1]["cities"][0]
+                result_div.innerHTML = getHtml(result_one) + getHtml(result_two);
+
+            } else {
+                console.log(q);
+            }
+        });
+
+
+
+});
+
+document.getElementById("resetButton").addEventListener("click", () => {
+    result_div.innerHTML ="";
+    document.getElementById("insertedSearch").value=""
+
+});
+
+function getHtml(result) {
+    return ` 
+    <div class="box" style="background-color:white" id="prodcolor">   
+        <h1>${result.name}</h1>
+        <img src=${result.imageUrl} >
+        <div >   
+        <span style="display:none"> Sample</span>   
+        <p>${result.description}</p>
+        </div>  
+     </div>
+      `}
+
